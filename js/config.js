3abce8aadbd6cb3555912ee77e0c1d23df1a8638
693850 <1522607396@qq.com>
2026-01-04 21:34:16 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,20 @@
+var BaseUrl =  "http://171.80.3.187:1786";
+var ResourceUrl = 'http://171.80.3.187:1999/ttxindasheng/';
+
+if(window.location.pathname!='/'
+    &&window.location.pathname!='/index.html'
+    &&window.location.pathname!='/site/login.html'
+    &&window.location.pathname!='/site/wxredd.html')
+{
+    var wwid=window.localStorage.getItem("wwid");
+    if(!wwid)
+    {
+        localStorage.setItem('enter_url',encodeURIComponent(window.location.href))
+
+        alert('请先登录');
+        window.location.href = '/index.html';
+    }
+}
+
+
+        
